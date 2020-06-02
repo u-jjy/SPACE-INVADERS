e292849a41ef2f9f8f6ec544fd4eb71883f0da18
UJJY <ujj.aggarwal@gmail.com>
2020-06-02 20:10:26 +0400

    Game over screen needs debugging

--- Space Invaders.py
+++ Space Invaders.py
@@ -90,6 +90,7 @@ class Enemy:
             self.lives = 3
         if difficulty == 'H':
             self.lives = 4
+        self.lives2 = self.lives
 
     def draw_enemy(self):
         if score_value >= self.min_score and self.lives > 0:
@@ -225,7 +226,7 @@ space_invader15 = Enemy(10)
 bullet = Bullet()
 start_game_button = Button(200, 430, (0, 255, 0), 'Start Game', 206, 440)
 quit_button = Button(530, 430, (255, 0, 0), 'Quit Game', 533, 440)
-
+play_again_button = Button(300, 430, (0, 255, 0), 'Play Again', 306, 440)
 
 # List of enemies
 enemies = [space_invader, space_invader2, space_invader3, space_invader4, space_invader5, space_invader6,
@@ -244,11 +245,11 @@ game_over_x = 273
 game_over_y = -200
 game_over_final_y = 216
 
-
 # Game Intro
 intro_title = pygame.image.load('space_invaders_title.png')
 intro_title_x = 150
 intro_title_y = 187
+
 
 def display_score():
     # Creating variable for PyGame surface. Contains text rendered using our loaded font variable
@@ -265,34 +266,52 @@ def check_position(enemy):
         game_over = True
 
 
+already_done = False  # Variable to prevent repeated subtraction from quit_button.x
+
+
 # Defining function to draw game window
 def drawGameWindow():
-    # Bringing game over screen if game_over
-    if game_over:
+    global already_done
+    if not game_begun:
         screen.blit(bg, (0, 0))
-        pygame.time.wait(195)
-        screen.blit(game_over_image, (game_over_x, game_over_y))
-        pygame.display.update()
-    else:
-        # screen.fill((48, 20, 58))
-        screen.blit(bg, (0, 0))
-        # Drawing bullet if it has been fired
+        screen.blit(intro_title, (intro_title_x, intro_title_y))
         start_game_button.draw_button()
         quit_button.draw_button()
-        if bullet.motion:
-            bullet.draw_bullet()
-        spaceship.draw_player()  # Drawing player
-        # Calling draw enemy method on every enemy, only works if they are activated
-        for enemy in enemies:
-            enemy.draw_enemy()
-        screen.blit(intro_title, (intro_title_x, intro_title_y))
-        display_score()
         pygame.display.update()
+    else:
+        # Bringing game over screen if game_over
+        if game_over:
+            screen.blit(bg, (0, 0))
+            if game_over_y > 210:
+                if not already_done:
+                    quit_button.x -= 100
+                    quit_button.text_x -= 100
+                    already_done = True
+                quit_button.draw_button()
+                play_again_button.draw_button()
+            if game_over_y < 210:
+                pygame.time.wait(195)
+            screen.blit(game_over_image, (game_over_x, game_over_y))
+            pygame.display.update()
+        else:
+            # screen.fill((48, 20, 58))
+            screen.blit(bg, (0, 0))
+            # Drawing bullet if it has been fired
+            if bullet.motion:
+                bullet.draw_bullet()
+            spaceship.draw_player()  # Drawing player
+            # Calling draw enemy method on every enemy, only works if they are activated
+            for enemy in enemies:
+                enemy.draw_enemy()
+            display_score()
+            pygame.display.update()
 
 
 # Main game loop
 game_won = False
 game_over = False
+game_begun = False
+played_again = False
 run = True
 while run:
     clock.tick(45)  # Using clock to not allow more than 45 iterations a second
@@ -303,55 +322,17 @@ while run:
 
         mouse_pos = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if start_game_button.mouse_is_over(mouse_pos):
-                print('START GAME')
-            if quit_button.mouse_is_over(mouse_pos):
-                run = False
-
-    keys = pygame.key.get_pressed()  # Creating a tuple of all the keys that are pressed, every iteration
-    if keys[pygame.K_RIGHT]:  # Checking if right key was pressed
-        if spaceship.x < (800 - spaceship.width - 4):
-            spaceship.move_right()  # Using method to move spaceship right
-    if keys[pygame.K_LEFT]:
-        if spaceship.x > 0:
-            spaceship.move_left()
-
-    # Bullet Movement
-    if bullet.motion:
-        # Subtracting from bullet y to make it go up consistently
-        bullet.y -= bullet.y_vel
-    if bullet.y <= 0 - bullet.height:
-        bullet.reset()  # Resetting bullet to original state to allow for re-shooting
-    if not bullet.motion:
-        bullet.update_x()  # Updating x value of bullet to correspond with ship every iteration
-    if keys[pygame.K_SPACE]:
-        bullet.fire_bullet()
-
-    # All enemy functions:
-    for enemy in enemies:
-        # Calling 'move_enemy' method to move invader left to right and down
-        enemy.move_enemy()
-        # Checking for collision
-        enemy.check_bullet_collision()
-        #  Respawning enemy and resetting bullet if collision is true using bullet_collision method
-        enemy.bullet_collision()
-
-    # Checking if any enemy has collided with ship
-    if spaceship.check_enemy_collision():
-        game_over = True
-
-    # Checking if any enemy has been let off screen
-    for enemy in enemies:
-        check_position(enemy)
-
-    # Checking if number of lives for every enemy, adding to dead if lives = 0
-    dead = 0  # Assigning dead variable
-    for enemy in enemies:
-        if enemy.lives == 0:
-            dead += 1
-
-    if dead == 15:  # Checking if all enemies are dead
-        game_won = True  # Making game won true
+            if start_game_button.mouse_is_over(mouse_pos) and not game_begun and not game_over:
+                game_begun = True
+            if quit_button.mouse_is_over(mouse_pos) and not game_begun or game_over:
+                if not play_again_button.mouse_is_over(mouse_pos):
+                    run = False
+            if play_again_button.mouse_is_over(mouse_pos) and game_over:
+                # for enemy in enemies:
+                #     enemy.lives = enemy.lives2
+                # score_value = 0
+                game_over = False
+                played_again = True
 
     if game_over:
         if not played_already:
@@ -360,6 +341,62 @@ while run:
             played_already = True
         if game_over_y < game_over_final_y:
             game_over_y += 20
+
+    if game_begun and not game_over:
+        if played_again:
+            pygame.mixer.music.unpause()
+            for enemy in enemies:
+                enemy.respawn()
+            dead = 0
+            game_over_y = -200
+            played_already = False
+            played_again = False
+
+        keys = pygame.key.get_pressed()  # Creating a tuple of all the keys that are pressed, every iteration
+        if keys[pygame.K_RIGHT]:  # Checking if right key was pressed
+            if spaceship.x < (800 - spaceship.width - 4):
+                spaceship.move_right()  # Using method to move spaceship right
+        if keys[pygame.K_LEFT]:
+            if spaceship.x > 0:
+                spaceship.move_left()
+
+        # Bullet Movement
+        if bullet.motion:
+            # Subtracting from bullet y to make it go up consistently
+            bullet.y -= bullet.y_vel
+        if bullet.y <= 0 - bullet.height:
+            bullet.reset()  # Resetting bullet to original state to allow for re-shooting
+        if not bullet.motion:
+            bullet.update_x()  # Updating x value of bullet to correspond with ship every iteration
+        if keys[pygame.K_SPACE]:
+            bullet.fire_bullet()
+
+        # All enemy functions:
+        for enemy in enemies:
+            # Calling 'move_enemy' method to move invader left to right and down
+            enemy.move_enemy()
+            # Checking for collision
+            enemy.check_bullet_collision()
+            #  Respawning enemy and resetting bullet if collision is true using bullet_collision method
+            enemy.bullet_collision()
+
+        # Checking if any enemy has collided with ship
+        if spaceship.check_enemy_collision():
+            game_over = True
+
+        # Checking if any enemy has been let off screen
+        for enemy in enemies:
+            check_position(enemy)
+
+        # Checking if number of lives for every enemy, adding to dead if lives = 0
+        dead = 0  # Assigning dead variable
+        for enemy in enemies:
+            if enemy.lives == 0:
+                dead += 1
+
+        if dead == 15:  # Checking if all enemies are dead
+            game_won = True  # Making game won true
+
 
     drawGameWindow()
 
